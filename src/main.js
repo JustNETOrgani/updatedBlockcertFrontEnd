71d6cf31531eed837657700879158e8837b4ec58
Jess <2482003411@qq.com>
2020-03-05 09:17:35 +0800

    图片上传

--- src/main.js
+++ src/main.js
@@ -5,9 +5,8 @@ import App from './App.vue'
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 
 new Vue({
   el: '#app',
