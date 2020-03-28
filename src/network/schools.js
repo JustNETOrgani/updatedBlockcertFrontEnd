3506c3d42e70471d32a98edae85302c03337c93b
JustNETlab <justnetonline@outlook.com>
2020-03-28 14:32:11 +0800

    School registration frontend partially implemented but register interface fails

--- src/network/schools.js
+++ src/network/schools.js
@@ -1,16 +1,33 @@
 import {request} from './request'
+import {fileRequest} from './request'
 
 export function Schlogin(data) {
   return request({
-    url: '/v1/api/school/login',
+    url: '/v1/api/schools/login',
+    method: 'post',
+    data: data
+  });
+}
+export function getSigImageDetails(data) {
+  return fileRequest({
+    url: '/v1/api/files/',
+    method: 'post',
+    data: data
+  });
+}
+
+export function getSchLogoDetails(data) {
+  return fileRequest({
+    url: '/v1/api/files/',
     method: 'post',
     data: data
   });
 }
 
 export function register(data) {
+  console.log("Data for registration: ", data)
   return request({
-    url:'/v1/api/school/register',
+    url:'/v1/api/schools/register',
     method: 'post',
     data: data
   });
